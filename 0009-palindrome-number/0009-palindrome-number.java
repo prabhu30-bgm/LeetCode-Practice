93eea907a7594a7a9743f84a5f9c6cfd4a3f75ea
Basavaprabhu Suresh Kudenatti <220516729+prabhu30-bgm@users.noreply.github.com>
2026-09-06 20:33:07 +0530

    Time: 5 ms (82.39%), Space: 46 MB (52.46%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -6,8 +6,8 @@ class Solution {
         int num = 0, temp = x;
 
         while(temp!=0){
-            int d = temp%10;
-            num = num * 10 + d;
+            int num1 = temp%10;
+            num = num * 10 + num1;
             temp = temp/10;
         }
         return x == num;
