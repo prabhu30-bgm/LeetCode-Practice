eae7360759874439744139b9754f267bf34d867c
Basavaprabhu Suresh Kudenatti <220516729+prabhu30-bgm@users.noreply.github.com>
2026-09-09 20:33:34 +0530

    Time: 3 ms (76.6%), Space: 46.7 MB (27.62%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int romanToInt(String s) {
+
+        int total = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+
+            int current = getValue(s.charAt(i));
+            int next = getValue(s.charAt(i + 1));
+
+            if (current < next) {
+                total -= current;
+            } else {
+                total += current;
+            }
+        }
+        total += getValue(s.charAt(s.length() - 1));
+
+        return total;
+    }
+
+    public int getValue(char c) {
+
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+}
