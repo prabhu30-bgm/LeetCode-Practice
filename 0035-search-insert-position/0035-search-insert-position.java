5a52e9c6673313e72ec0563eb80855dc6869ab4d
Basavaprabhu Suresh Kudenatti <220516729+prabhu30-bgm@users.noreply.github.com>
2026-09-11 23:09:32 +0530

    Time: 0 ms (100%), Space: 44.6 MB (66.49%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -5,7 +5,7 @@ class Solution {
 
         while(low <= high)
         {
-            int mid = low + (high - low) / 2;
+            int mid = (low + high) / 2;
 
             if( nums[mid] == target)
             {
