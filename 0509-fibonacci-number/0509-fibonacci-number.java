3a2e0d22b52e85ebe545e739fe5dedefd8daac1f
Basavaprabhu Suresh Kudenatti <220516729+prabhu30-bgm@users.noreply.github.com>
2026-09-18 23:20:23 +0530

    Time: 0 ms (100%), Space: 42.2 MB (33.52%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,6 +2,16 @@ class Solution {
     public int fib(int n) {
         if(n == 0 || n == 1)
             return n;
-        return fib(n-1)+fib(n-2);
+        // return fib(n-1)+fib(n-2);
+        int first = 1;
+        int second = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+
+            first = second;
+            second = third;
+        }
+        return first;
     }
 }
