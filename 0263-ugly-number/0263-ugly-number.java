51b9e4ccc2a0522e8a0af80586ce03eb89462ef5
Basavaprabhu Suresh Kudenatti <220516729+prabhu30-bgm@users.noreply.github.com>
2026-09-26 23:11:13 +0530

    Time: 1 ms (99.15%), Space: 42.6 MB (50.69%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n == 1)
+            return true;
+        
+        for(int i = 2; i < 6 && n > 0; i++)
+        {
+            while(n % i == 0)
+            {
+                n /= i ;
+            }
+        }
+        return n == 1;
+    }
+}
