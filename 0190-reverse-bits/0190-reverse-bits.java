e336cf18ebc23ba85ba91f5e209446c1d6249a98
Basavaprabhu Suresh Kudenatti <220516729+prabhu30-bgm@users.noreply.github.com>
2026-09-26 23:52:11 +0530

    Time: 5 ms (5.02%), Space: 43.3 MB (5.19%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,5 +1,7 @@
 class Solution {
     public int reverseBits(int n) {
+        //When i was first saw this problem, i thought this program may take time to solve because of converting integer to binary then binary to reverse and then back to integer but in JAVA we have to many built-in functions that helped me to solve this problem within 10-15 minutes.
+
         String Str = Integer.toBinaryString(n);
         while( Str.length() < 32) 
         { 
